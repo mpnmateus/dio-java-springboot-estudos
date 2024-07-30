02e8afed523cce90876c531b7b33e649400b3d8b
mateusnovo <mpnmateus@gmail.com>
2024-07-30 11:24:14 -0300

    feat: incluindo primeiras funcionalidades

---
+++ src/desafios/ContaTerminal.java
@@ -0,0 +1,19 @@
+package desafios;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        /** Lista TO-DO
+        *1. Conhecer e importar a classe Scanner
+        *2. Exibir as mensagens para nosso usuário
+        *3. Obter, pelo Scanner, os valores digitados no terminal
+        *4. Exibir a mensagem da conta criada
+        */
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+
+    }
+}
