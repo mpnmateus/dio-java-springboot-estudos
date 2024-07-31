aec4231857e6dd3c5f0f4f0e7e6ba6d203cc41d7
mateusnovo <mpnmateus@gmail.com>
2024-07-31 16:19:05 -0300

    feat: criando algoritmo que seleciona candidatos num array conforme uma condicao. caso satisfaca, coloca em outro array. imprime apenas posicoes nao nulas

---
+++ src/javabasico/AAexercicios/candidatura/ProcessoSeletivoDois.java
@@ -0,0 +1,36 @@
+package javabasico.AAexercicios.candidatura;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class ProcessoSeletivoDois {
+    public static void main(String[] args) {
+        selecionarCandidatos();
+    }
+    static void selecionarCandidatos(){
+        double valorBaseSalarial = 2000;
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        String [] candidatosSelecionados = new String[candidatos.length];
+
+        double salarioPretendido = 0;
+        int quantidadeCandidatos = 0;
+        while(quantidadeCandidatos <= 5) {
+            for (int i = 0; i < candidatos.length; i++) {
+                salarioPretendido = valorPretendido();
+                if (salarioPretendido <= valorBaseSalarial) {
+                    //System.out.println(candidatos[i]);
+                    candidatosSelecionados[i] = candidatos [i];
+                }
+                quantidadeCandidatos++;
+            }
+        }
+        System.out.println("\nA lista de candidatos selecionados é: ");
+        for (int i = 0; i < candidatos.length; i++) {
+            if (candidatosSelecionados[i] != null) {
+                System.out.println(candidatosSelecionados[i]);
+            }
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+}
