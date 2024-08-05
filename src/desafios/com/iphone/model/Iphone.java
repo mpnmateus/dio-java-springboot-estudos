b27ff90101796a1bf3dc32883c3c0f734a7a4431
mateusnovo <mpnmateus@gmail.com>
2024-08-05 15:40:39 -0300

    metodos da interface implementados

--- src/desafios/com/iphone/model/Iphone.java
+++ src/desafios/com/iphone/model/Iphone.java
@@ -1,11 +1,11 @@
 package desafios.com.iphone.model;
 
-public class Iphone implements AparelhoTelefonico, ReprodutorMusical{
+public class Iphone implements AparelhoTelefonico, ReprodutorMusical, NavegadorInternet{
 
     //Métodos relacionados ao aparelho telefônico
     @Override
     public void ligar(String numero){
-        System.out.println("Ligando para o número "+ numero + "através do iphone..");
+        System.out.println("Ligando para o número "+ numero + " através do iphone..");
     }
     @Override
     public void atender(){
@@ -16,6 +16,7 @@ public class Iphone implements AparelhoTelefonico, ReprodutorMusical{
         System.out.println("Iniciando correio de voz a partir do iphone..");
     }
 
+    //Métodos relacionados ao reprodutor musical
     @Override
     public void tocar(){
         System.out.println("Tocando mídia a partir do iphone.");
@@ -26,6 +27,22 @@ public class Iphone implements AparelhoTelefonico, ReprodutorMusical{
     }
     @Override
     public void selecionarMusica(String musica){
-        System.out.println("");
+        System.out.println("Reproduzindo a música: " + musica + " através do iphone.");
     }
+
+    //Métodos relacionados ao navegador de internet
+    @Override
+    public void exibirPagina(String url){
+        System.out.println("Exibindo a página '"+ url + "'");
+    }
+    @Override
+    public void adicionarNovaAba(){
+        System.out.println("Adicionando nova aba.");
+    }
+    @Override
+    public void atualizarPagina(){
+        System.out.println("Atualizando página.");
+    }
+
 }
+
